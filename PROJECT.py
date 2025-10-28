0078c7df095d95053c4ea2a23c8db9aab9a1ae44
yahav <yahavmichael51@gmail.com>
2025-10-28 17:51:42 +0200

    NEW_PROJECT

---
+++ PROJECT.py
@@ -0,0 +1,56 @@
+liked_songs = {
+    "Shake It Off": {
+        "artist": "Taylor Swift",
+        "duration": (3, 23),
+        "genre": "Pop"
+    },
+    "Shemesh": {
+        "artist": "Mergi",
+        "duration": (2, 33),
+        "genre": "Israeli"
+    },
+    "Chop Suey!": {
+        "artist": "System of a Down",
+        "duration": (3, 30),
+        "genre": "Metal"
+    },
+    "Mimaamakim": {
+        "artist": "Idan Raichel",
+        "duration": (4, 33),
+        "genre": "Israeli"
+    },
+    "Do I Wanna Know?": {
+        "artist": "Arctic Monkeys",
+        "duration": (4, 26),
+        "genre": "Rock"
+    },
+    "Love Story": {
+        "artist": "Taylor Swift",
+        "duration": (3, 55),
+        "genre": "Pop"
+    },
+    "Bo’ee": {
+        "artist": "Idan Raichel",
+        "duration": (4, 45),
+        "genre": "Israeli"
+    },
+    "TIL FURTHER NOTICE": {
+        "artist": "Travis",
+        "duration": (4, 45),
+        "genre": "Rap"
+    },
+    "Ghost town": {
+        "artist": "Kanye",
+        "duration": (2, 45),
+        "genre": "Rap"
+    },
+    "Over": {
+        "artist": "carti",
+        "duration": (1, 45),
+        "genre": "rap"
+    }
+}
+
+
+def main():
+    pass
